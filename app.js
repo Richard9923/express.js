a8c3ec0b2735d75c7b0f74d2f8648fe0ec9c785e
Richard <programmingislife23@gmail.com>
2024-07-07 20:26:55 -0300

    primeiros passos com express

--- app.js
+++ app.js
@@ -21,8 +21,15 @@ let posts = [
     {id: 3, title: "Post three"}
 ];
 
+// Get all posts
 app.get('/api/posts', (req, res) => {
     res.json(posts);
+})
+
+// Get single post
+app.get('/api/posts/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    res.json(posts.filter((post) => post.id === id));
 })
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
