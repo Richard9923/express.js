6636deed4ed4b3e3ff75575f2df9faf171da9655
Richard <programmingislife23@gmail.com>
2024-07-08 21:06:35 -0300

    front end trabalhando junto com o back end

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 const output = document.querySelector("#output");
 const button = document.querySelector("#get-posts-btn");
+const form = document.querySelector("#add-post-form");
 
 
 async function showPosts() {
@@ -26,5 +27,36 @@ async function showPosts() {
    
 }
 
+// Submit new post
+async function addPost(e) {
+    e.preventDefault();
+    const formData = new FormData(this);
+    const title = formData.get('title');
+
+    try {
+        const res = await fetch('http://localhost:8000/api/posts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({title})
+        })
+        if (!res.ok) {
+            throw new Error('Failed to add post');
+        }
+   
+        const newPost = await res.json();
+
+        const postEl = document.createElement('div');
+        postEl.textContent = newPost.title;
+        output.appendChild(postEl);
+        showPosts();
+
+    } catch (error) {
+        console.error('Error adding post');
+    }
+}
+
 // Event listeners
 button.addEventListener('click', showPosts);
+form.addEventListener('submit', addPost);
